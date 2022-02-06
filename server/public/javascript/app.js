106968a2eff0d2f86afa8565a2d4481085173e5c
aaron <aaron@pop-os.localdomain>
2022-02-06 17:16:58 +0000

    ++ /server/views/icons >> add svg icons for CRUD:UD controls

--- server/public/javascript/app.js
+++ server/public/javascript/app.js
@@ -3,20 +3,26 @@ class Source {
         this.source = _source;
         this.strID = this.source.id;
         this.id = this.strID.slice(2)
-        this.toggle = document.querySelector(`#${this.strID} .options`);
-        this.toggle.addEventListener('click', () => this.toggleMenu());
+        this.menu = document.querySelector(`#${this.strID} .options`);
+        this.menu.addEventListener('pointerover', () => this.toggleMenuOn());
+        this.menu.addEventListener('pointerleave', () => this.toggleMenuOff())
         
         this.delete = document.querySelector(`#${this.strID} form.delete`);
         this.deleteCtl = [...this.delete].filter(i => Source.isSubmitBtn(i))[0];
+
         this.deleteCtl.addEventListener('click', evt => {
             evt.preventDefault();
             if (confirm('Are you sure?')) this.delete.submit();
         });
     };
 
-    toggleMenu() {
-        return this.toggle.classList.toggle('show');
+    toggleMenuOn() {
+        return this.menu.classList.add('show');
     };
+
+    toggleMenuOff() {
+        return this.menu.classList.remove('show');
+    }
 
     static isSubmitBtn(btn) {
         return btn.nodeName === 'BUTTON' && btn.type === "submit";
